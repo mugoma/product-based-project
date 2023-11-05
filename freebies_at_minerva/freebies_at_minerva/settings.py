e75501f78caadd592efaf5f9675311909334924f
Okomba Mugoma <mugoma90@uni.minerva.edu>
2023-11-04 17:28:07 -0700

    Add freebies & notification apps to settings

--- freebies_at_minerva/freebies_at_minerva/settings.py
+++ freebies_at_minerva/freebies_at_minerva/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth.apps.AuthConfig'
+    'authentication.apps.AuthenticationConfig',
+    'freebies.apps.FreebiesConfig',
+    'notifications.apps.NotificationsConfig'
 ]
 
 MIDDLEWARE = [
@@ -124,4 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = "auth.User"
+AUTH_USER_MODEL = "authentication.User"
