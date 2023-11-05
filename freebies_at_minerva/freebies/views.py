ddc7f6ce076c7d813d4a14d6593e09c1592b764a
Okomba Mugoma <mugoma90@uni.minerva.edu>
2023-11-04 18:04:35 -0700

    Add class-based views for CRUD operations.

--- freebies_at_minerva/freebies/views.py
+++ freebies_at_minerva/freebies/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+
+
+from .models import Freebie
 
 # Create your views here.
+
+
+class FreebiesCreateView(CreateView):
+    model = Freebie
+    template_name = 'freebies/form.html'
+
+    def get_success_url(self) -> str:
+        return reverse('freebies:detail', args={"pk": self.get_object().pk})
+
+
+class FreebiesListView(ListView):
+    model = Freebie
+    template_name = 'freebies/list.html'
+
+
+class FreebiesDetailView(DetailView):
+    model = Freebie
+    template_name = 'freebies/detail.html'
+
+
+class FreebiesUpdateView(UpdateView):
+    model = Freebie
+    template_name = 'freebies/form.html'
+
+    def get_success_url(self) -> str:
+        return reverse('freebies:detail', args={"pk": self.get_object().pk})
+
+
+class FreebiesDeleteView(DeleteView):
+    model = Freebie
+    template_name = 'freebies/delete.html'
+    form_class = None
+
+    success_url = reverse_lazy('freebies:list')
