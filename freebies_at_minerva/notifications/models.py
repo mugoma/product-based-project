87ea98130e93115f337e46750a84f40ba0ada6af
Okomba Mugoma <mugoma90@uni.minerva.edu>
2023-11-05 14:59:25 -0800

    Add city notifications model

--- freebies_at_minerva/notifications/models.py
+++ freebies_at_minerva/notifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from freebies.models import CITIES
 
 # Create your models here.
+
+
+class CityNotifications(models.Model):
+    city = models.TextField(choices=CITIES)
+    user = models.ForeignKey('authentication.user',
+                             models.CASCADE, null=True, blank=True)
