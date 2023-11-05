c40312b2759300d5a0754ff0325667e0a0ccf829
Okomba Mugoma <mugoma90@uni.minerva.edu>
2023-11-05 11:28:55 -0800

    Add templates directory

--- freebies_at_minerva/freebies_at_minerva/settings.py
+++ freebies_at_minerva/freebies_at_minerva/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'freebies_at_minerva.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
